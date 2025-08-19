ea9b42f6c964b3c3236af738a22739e43cfc433c
poxicatalin <pocsanjr@proton.me>
2025-08-19 12:34:43 +0300

    improve factory by using laravel service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\Payments\PaypalPaymentService;
+use App\Services\Payments\StripePaymentService;
+use App\Services\Payments\CashOnDeliveryService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +15,17 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind('stripe.adapter', function ($app) {
+            return new StripePaymentService();
+        });
+
+        $this->app->bind('paypal.adapter', function ($app) {
+            return new PaypalPaymentService();
+        });
+
+        $this->app->bind('cod.adapter', function ($app) {
+            return new CashOnDeliveryService();
+        });
     }
 
     /**
--- app/Services/Payments/PaymentGatewayFactory.php
+++ app/Services/Payments/PaymentGatewayFactory.php
@@ -3,18 +3,21 @@
 namespace App\Services\Payments;
 
 use RuntimeException;
+use Illuminate\Contracts\Container\Container;
 use App\Services\Payments\PaypalPaymentService;
 use App\Services\Payments\StripePaymentService;
 use App\Services\Payments\CashOnDeliveryService;
 
 class PaymentGatewayFactory
 {
+    public function __construct(private readonly Container $container) {}
+
     public function make(string $gateway)
     {
         return match ($gateway) {
-            'stripe' => new StripePaymentService(),
-            'paypal' => new PaypalPaymentService(),
-            'cod' => new CashOnDeliveryService(),
+            'stripe' => $this->container->make('stripe.adapter'),
+            'paypal' => $this->container->make('paypal.adapter'),
+            'cod' => $this->container->make('cod.adapter'),
             default => throw new RuntimeException("Invalid payment gateway"),
         };
     }
